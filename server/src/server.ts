5a17199f797474601f39a61c770931dbb0fdb964
Ariel Lima <ariellima2314@gmail.com>
2022-09-12 16:07:57 -0300

    #

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/ads', (request, response) => {
+app.get("/ads", (request, response) => {
   return response.json([
-    { id: 1, name: 'Ad 1' },
-    { id: 2, name: 'Ad 2' },
-    { id: 3, name: 'Ad 3' },
-  ])
-})
+    { id: 1, name: "Ad 1" },
+    { id: 2, name: "Ad 2" },
+    { id: 3, name: "Ad 3" },
+    { id: 4, name: "Ad 4" },
+  ]);
+});
 
-app.listen(3333)
+app.listen(3333);
