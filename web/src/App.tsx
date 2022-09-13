ee5068c9c62dca3a4e120aee7229392e3cd864b6
Ariel Lima <72455661+whyariel2@users.noreply.github.com>
2022-09-13 20:36:09 -0300

    landingpage 2-6 web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,7 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css';
+
 import logoImg from './assets/logo-nlw-esports.svg';
 
 
@@ -45,7 +48,7 @@ function App() {
           <img src="/game4.png" alt="" />
 
           <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <strong className='font-bold text-white block'>Valorant</strong>
             <span className='text-zinc-300 text-sm block'>4 anúncios</span>
           </div>
         </a>
@@ -55,12 +58,12 @@ function App() {
           <img src="/game5.png" alt="" />
 
           <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
-            <strong className='font-bold text-white block'>Dota 2</strong>
+            <strong className='font-bold text-white block'>Minecraft</strong>
             <span className='text-zinc-300 text-sm block'>4 anúncios</span>
           </div>
         </a>
 
-        
+
         <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game6.png" alt="" />
 
@@ -69,6 +72,20 @@ function App() {
             <span className='text-zinc-300 text-sm block'>4 anúncios</span>
           </div>
         </a>
+      </div>
+
+      <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 font-medium bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   );
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,13 +5,20 @@ module.exports = {
     './index.html',
   ],
   theme: {
+
+    // Change default font-family
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage:{
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 30.08%, #43E7AD 51.94%, #E1D55D 19.57%)',
+        'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
   plugins: [],
 }
+ ;
+
