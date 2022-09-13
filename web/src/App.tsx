ffbf307b34c95d5463334a53e3d975ac7d19ff62
Ariel Lima <72455661+whyariel2@users.noreply.github.com>
2022-09-13 18:10:07 -0300

    box all done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,23 +11,63 @@ function App() {
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent '>duo</span>  está aqui.</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="#">
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game1.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href="#">
+
+
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game2.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href="#">
+
+
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game3.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>Counter Strike</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href="#">
+
+
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game4.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href="#">
+
+
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game5.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href="#">
+
+        
+        <a href="#" className='relative rounded-lg overflow-hidden'>
           <img src="/game6.png" alt="" />
+
+          <div className='w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
       </div>
     </div>
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,8 +8,8 @@ module.exports = {
     extend: {
       backgroundImage:{
         galaxy: "url('/background-galaxy.png')",
-        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 30.08%, #43E7AD 51.94%, #E1D55D 19.57%)'
-
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 30.08%, #43E7AD 51.94%, #E1D55D 19.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
