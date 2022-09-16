cd545c7b57b274e2d28f6323efa985f549e06b7b
Ariel Lima <ariellima2314@gmail.com>
2022-09-16 15:31:18 -0300

    start db

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,13 +14,21 @@ app.post('/ads', (request, response) =>{
 
 });
 
-app.get("/ads", (request, response) => {
+app.get("/games/:id/ads", (request, response) => {
+  // const adId = request.params.id;
+
   return response.json([
     { id: 1, name: "Ad 1" },
     { id: 2, name: "Ad 2" },
     { id: 3, name: "Ad 3" },
     { id: 4, name: "Ad 4" },
   ]);
+});
+
+app.get("/games/:id/discord", (request, response) => {
+  // const gameId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
