9bef1bc9d4e4f18063b8106aa62243fe0a390f9a
Ariel Lima <72455661+whyariel2@users.noreply.github.com>
2022-09-18 20:36:12 -0300

    #

---
+++ server/README.md
@@ -0,0 +1,18 @@
+### === Game ===
+
+id
+title
+bannerUrl
+
+### === Ad ===
+
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,92 +1,35 @@
+import { useState } from 'react';
+
 import { MagnifyingGlassPlus } from 'phosphor-react'
 
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg';
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 
 //This is a component
 function App() {
+  const [hasUserClickedOnButton, setHasUserClickedOnButton] = useState(false)
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="" />
 
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='bg-nlw-gradient bg-clip-text text-transparent '>duo</span>  está aqui.</h1>
 
+      {/* Container Game Banner */}
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game1.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game2.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Apex Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game3.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Counter Strike</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game4.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Valorant</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game5.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Minecraft</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-
-
-        <a href="#" className='relative rounded-lg overflow-hidden'>
-          <img src="/game6.png" alt="" />
-
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>Fortnite</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
+        <GameBanner bannerUrl='/game1.png' title='League of Legends' adsCount={5}/>
+        <GameBanner bannerUrl='/game2.png' title='Apex Legends' adsCount={5}/>
+        <GameBanner bannerUrl='/game3.png' title='CS:GO' adsCount={5}/>
+        <GameBanner bannerUrl='/game4.png' title='Valorant' adsCount={5}/>
+        <GameBanner bannerUrl='/game5.png' title='Minecraft' adsCount={5}/>
+        <GameBanner bannerUrl='/game6.png' title='Fortnite' adsCount={5}/>
       </div>
 
-      <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 font-medium bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
+      <CreateAdBanner />
     </div>
   );
 }
